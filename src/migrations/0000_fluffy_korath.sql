18f135cf10de2741611224212c2a33508adb9b8b
aviliomoz <aviliomuoz@gmail.com>
2024-06-25 10:27:56 -0500

    clean migrations

--- src/migrations/0000_fluffy_korath.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS "users" (
-	"id" uuid PRIMARY KEY NOT NULL,
-	"name" varchar(30) NOT NULL,
-	"email" varchar(20) NOT NULL,
-	"password" varchar(20) NOT NULL
-);
--- src/migrations/0003_giant_freak.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "users" ALTER COLUMN "name" SET DATA TYPE varchar;--> statement-breakpoint
-ALTER TABLE "users" ALTER COLUMN "email" SET DATA TYPE varchar;--> statement-breakpoint
-ALTER TABLE "users" ALTER COLUMN "password" SET DATA TYPE varchar;
